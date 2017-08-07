e02a376d4492030f33a28bb19fa8fe7de7c59040
balasekar <baalasekar@gmail.com>
2017-08-07 22:59:35 +0100

    SPZ-NINJA - Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    state = {users: []}
+
+    componentDidMount() {
+        fetch('/users')
+            .then(res => res.json())
+            .then(users => this.setState({users}));
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to SPZ</h2>
+                </div>
+                <p className="App-intro">
+                    {this.state.users.map(user =>
+                        <div key={user.id}>{user.username}</div>
+                    )}
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
