5afd88437856a369c5e0931d6eaf3bdb36e33e82
balasekar <baalasekar@gmail.com>
2017-08-26 10:54:09 +0100

    React - Async

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { browserHistory } from 'react-router'
-import { syncHistoryWithStore } from 'react-router-redux'
-import Root from './containers/Root'
-import configureStore from './store/configureStore'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import reducer from './reducers'
+import App from './containers/App'
 
-const store = configureStore()
-const history = syncHistoryWithStore(browserHistory, store)
+const middleware = [ thunk ]
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+)
 
 render(
-  <Root store={store} history={history} />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
